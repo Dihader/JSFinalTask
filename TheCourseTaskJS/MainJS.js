d795cd50b7f31e286533b7a51c2e387fa26746b1
Vladimir Dolgy <diastriker@list.ru>
2016-02-15 13:12:02 +0300

    NO JqueryPlugin

---
+++ TheCourseTaskJS/MainJS.js
@@ -0,0 +1,123 @@
+﻿
+
+//var cbxTypeSelect = document.getElementById("cbxTypeSelect");
+//cbxTypeSelect.addEventListener("change", showTypeControls);
+
+//var btnAddControls = document.getElementById("btnAddControls");
+//btnAddControls.addEventListener("click", createNewControls);
+
+function showTypeControls() {
+    var typeIndex = document.getElementById("cbxTypeSelect").selectedIndex;
+    var tdParts = document.getElementById("tdForParts");
+
+    switch (typeIndex) {
+        case 0: postControl(tdParts); break;
+        case 1: phoneControl(tdParts); break;
+        case 2: exponentialNotationControl(tdParts); break;
+        case 3: numbersControl(tdParts); break;
+    }
+
+}
+
+function phoneControl(tdParts) {
+    tdParts.style.visibility = "hidden";
+
+}
+function postControl(tdParts) {
+    tdParts.style.visibility = "visible";
+}
+function exponentialNotationControl(tdParts) {
+    tdParts.style.visibility = "hidden";
+}
+function numbersControl(tdParts) {
+    tdParts.style.visibility = "hidden";
+}
+
+function createNewControls() {
+    var typeIndex = document.getElementById("cbxTypeSelect").selectedIndex;
+    if (typeIndex == 0 && checkNumberParts()) {
+        createElementsWithDiv();
+    }
+    else if (typeIndex != 0) {
+        createElementsWithDiv();
+    }
+    else {
+    }
+
+}
+function checkNumberParts() {
+    var valuePartsCounter = document.getElementById('txtPartsCounter').value;
+    var isNumber = /^\d+$/.test(valuePartsCounter);
+    if (isNumber && valuePartsCounter > 2) {
+        return true;
+    }
+    else {
+        alert('Количество частей должно быть числом, больше 2!');
+        return false;
+    }
+}
+function createElementsWithDiv() {
+    var newDiv = document.createElement('div');
+    document.body.appendChild(newDiv);
+    createLabelType(newDiv);
+    createInputTypeText(newDiv);
+    createButtonOk(newDiv);
+    createLabelResult(newDiv);
+    createButtonDelete(newDiv);
+}
+function createLabelType(newDiv) {
+    var lblType = document.createElement('label');
+    var typeIndex = document.getElementById("cbxTypeSelect").selectedIndex;
+    var typeMessage = document.getElementById("cbxTypeSelect").options[typeIndex].text;
+    lblType.innerText = typeMessage;
+    newDiv.appendChild(lblType);
+}
+function createInputTypeText(newDiv) {
+    var newInput = document.createElement('input');
+    var valueMask = document.getElementById('txtMask').value;
+    newInput.placeholder = valueMask;
+    newDiv.appendChild(newInput);
+    newInput.addEventListener("keypress", function () { maskTypeFilter(newInput) });
+}
+function createButtonOk(newDiv) {
+    var newButtonOk = document.createElement('button');
+    newButtonOk.innerText = "OK";
+    newDiv.appendChild(newButtonOk);
+
+}
+function createLabelResult(newDiv) {
+    var newResult = document.createElement('label');
+    newResult.innerText = "Результат:";
+    newDiv.appendChild(newResult);
+}
+function createButtonDelete(newDiv) {
+    var newButtonDelete = document.createElement('button');
+    newButtonDelete.innerText = "X";
+    newDiv.appendChild(newButtonDelete);
+    newButtonDelete.addEventListener("click", function () { deleteDiv(newDiv) });
+}
+function deleteDiv(newDiv) {
+    document.body.removeChild(newDiv);
+
+}
+function maskTypeFilter(newInput) {
+    var code = event.keyCode;
+    var mask = newInput.placeholder;
+    var text = String.fromCharCode(code);
+
+    if (text == mask[newInput.value.length] || mask[newInput.value.length] == '_') {
+        if (mask.length == newInput.value.length) {
+            alert('Ввод завершен!!!');
+        }
+    }
+    else {
+        alert('Вводите по маске!');
+        event.returnValue = false;
+        return false;
+    }
+}
+
+
+
+
+
---
+++ TheCourseTaskJS/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("TheCourseTaskJS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MICROSOFT")]
+[assembly: AssemblyProduct("TheCourseTaskJS")]
+[assembly: AssemblyCopyright("© MICROSOFT, 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("0db1e640-7656-4435-8d42-7c16c4551a14")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
